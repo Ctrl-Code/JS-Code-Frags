2b567b8cb4acb8cab0f22c124c26cd2583937d8f
ctrl-code <chebomsta@gmail.com>
2020-01-28 18:23:20 +0530

    ADDED object for testing

---
+++ object.js
@@ -0,0 +1,28 @@
+// sample object for understanding
+
+let ourObject = [
+    {
+        "North India":[
+            {'Haryana': ['Sonipat','Panipat']},
+            'Punjab',
+        ]
+    },
+    {
+        "South India":[
+            'Tamil Nadu',
+            {
+                'Karnataka':[{'Kormangla': ['North',{'South':['1st Main','2nd Main']}]}, 'Domlur']
+            },
+        ]
+    },
+    'East India',
+    {
+        'West India': [
+            {
+                'GJ': [{"Gandhinagar":['area-1','area-2']}]   
+            }
+        ]
+    }
+];
+
+module.exports = ourObject;
