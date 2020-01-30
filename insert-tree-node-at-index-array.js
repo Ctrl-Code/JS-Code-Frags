d1735985aa2475fe315723cfc83ca6fbf512f44a
ctrl-code <chebomsta@gmail.com>
2020-01-30 16:01:54 +0530

    insert now adds at leaf node as well

--- insert-tree-node-at-index-array.js
+++ insert-tree-node-at-index-array.js
@@ -7,30 +7,42 @@ const object = require('./object');
 let getTreeNode = require('./get-tree-node-from-index-array');
 
 function insertTreeNodeAtIndexArray(object, jsonArray, objectNode){
-    let parsedArray = JSON.parse(jsonArray);
-
-    let insertionIndex = parsedArray.slice(-1)[0];
-    let processingArray = parsedArray.slice(0,-1);
-
-    let parentElement = getTreeNode(object, JSON.stringify(processingArray));
-
-    let parentElementLength = parentElement.length;
-
-    // insert the node
-    switch(parentElementLength >= 0){
-        case true: {
-            // is array
-            parentElement.splice(insertionIndex, 0, objectNode);
-        } break;
-
-        case false: {
-            // is object
-            Object.values(parentElement)[0].splice(insertionIndex, 0, objectNode);
-        } break;
+    let currentNode = getTreeNode(object, jsonArray)
+    let type = typeof(currentNode);
+    
+    let objectType = (currentNode) => {
+        currentNode[Object.keys(currentNode)[0]].push(objectNode);
+    },
+    stringType = (currentNode) => {
+        // get it's parent and remove it from the place and put the object instead
+        let parsedArray = JSON.parse(jsonArray);
+        let indexToBeRemoved = parsedArray.splice(-1,1)[0];
+        let parentNode = getTreeNode(object,JSON.stringify(parsedArray));
+        let parentArray = Object.values(parentNode)[0];
         
+        // creating a new Object to be inserted at it's place
+        let newObject = {}
+        newObject[currentNode] = [objectNode];
+
+        // form new array
+        let newArray = parentArray.map((item, index) =>{
+            if(index === indexToBeRemoved){
+                return newObject;
+            }
+            else{
+                return item;
+            }
+        });
+
+        // insert new array in element
+        parentNode[Object.keys(parentNode)[0]] = newArray;
+    }
+
+    switch(type){
+        case 'string': stringType(currentNode); break;
+        case 'object': objectType(currentNode); break;
         default: ;
     }
-    return undefined;
 }
 
 module.exports = insertTreeNodeAtIndexArray;
