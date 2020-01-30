7e092f3259a0f09c84261a7764e0161bbab350ed
ctrl-code <chebomsta@gmail.com>
2020-01-30 13:26:52 +0530

    get-tree-node RETURNS undefined if tree depth increased

--- get-tree-node-from-index-array.js
+++ get-tree-node-from-index-array.js
@@ -9,20 +9,28 @@ function getTreeNodeFromIndexArray(treeObject,stringifiedArray){
     let parsedArray = JSON.parse(stringifiedArray);
     
     function getNodeRecursively(obj, array){
-        if(array.length){
-            if(obj.length >= 0){
-                newObj = obj[array[0]];
-                newArray = array.slice(1);
-                return getNodeRecursively(newObj,newArray);
+
+        if(typeof(obj) === 'string' && array.length)
+            return undefined;
+
+        else{
+
+            if(array.length){
+                if(obj.length >= 0){
+                    let newObj = obj[array[0]];
+                    let newArray = array.slice(1);
+                    return getNodeRecursively(newObj,newArray);
+                }
+
+                else{
+                    let newObj = Object.values(obj)[0][array[0]];
+                    let newArray = array.slice(1);
+                    return getNodeRecursively(newObj, newArray);
+                }
             }
-            else{
-                newObj = Object.values(obj)[0][array[0]];
-                newArray = array.slice(1);
-                return getNodeRecursively(newObj, newArray);
-            }
-        }
-        else
+
             return obj;
+        }
     }
 
     let node = getNodeRecursively(treeObject, parsedArray);
